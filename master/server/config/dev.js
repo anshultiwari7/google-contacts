1001f8f4a411cccdb1a94dfb14e6d9f0e4d55e70
Anshul Tiwari <anshul.tiwari@ziploan.in>
2020-09-07 05:41:08 +0530

    master including client & server application

---
+++ master/server/config/dev.js
@@ -0,0 +1,10 @@
+module.exports = {
+  cookieKey: '47iZdxnBHaPxUdVkbGJ90HwR9Nj1UmUv',
+  googleClientID:
+    '614063924361-vevm3h8qs9oancpldj329c9kl2st7h31.apps.googleusercontent.com',
+  googleClientSecret: '0qmAzpQKwZl-djhJ258LuQGV',
+  mongoURI:
+    'mongodb+srv://umnk003:aEXLxBr3ahznn5Dt@clusterx-mxgxg.gcp.mongodb.net/test?retryWrites=true&w=majority',
+  frontEndURL: 'http://localhost:3000/#',
+  baseURL: 'http://localhost:3000',
+};
---
+++ master/server/config/prod.js
@@ -0,0 +1,8 @@
+module.exports = {
+  cookieKey: process.env.COOKIE_KEY,
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  frontEndURL: process.env.FRONT_END_URL,
+  baseURL: process.env.BASE_URL,
+};
